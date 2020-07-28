d2f1d6330f65b64aabf6cd018bc361de904544fa
Chris Ferguson <cfergus7@gmail.com>
2020-07-27 22:43:04 -0500

    not working

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div className="container">
+                <nav className="main-nav">
+                    <h1 className="main-nav__logo">
+                        <Link to="/"></Link>
+                    </h1>
+                    <ul className="main-nav__menu">
+                        <li className="main-nav__item">
+                            <Link className="main-nav__link" to="/projects">Projects</Link>
+                        </li>
+                        <li className="main-nav__item">
+                            <Link className="main-nav__link" to="/about">About</Link>
+                        </li>
+                        <li className="main-nav__item">
+                            <Link className="main-nav__link" to="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
+}
