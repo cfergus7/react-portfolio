e7759c1ff46888429c5cacdf2497a1777284b1e4
Chris Ferguson <cfergus7@gmail.com>
2020-07-27 23:41:51 -0500

    rewind

---
+++ build/precache-manifest.be597a3c2e910d5e20e5e6b39902621b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ac5da862c8e3ec36a8361bcddfa88a",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "70f65456c6a86fe6956a",
+    "url": "/myapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6240303912ba9da13740",
+    "url": "/myapp/static/js/2.0feb8754.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.0feb8754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70f65456c6a86fe6956a",
+    "url": "/myapp/static/js/main.4d4d523d.chunk.js"
+  },
+  {
+    "revision": "b63da6b423c162dd4bff",
+    "url": "/myapp/static/js/runtime-main.ab3d0e87.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
